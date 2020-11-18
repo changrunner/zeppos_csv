f72195e14eecbce477ccd5abf06d0e01eb47262d
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2020-11-18 08:40:46 -0600

    Fix bug on csv files to sql server so it does not create sql table everytime.

--- zeppos_csv/csv_files.py
+++ zeppos_csv/csv_files.py
@@ -11,12 +11,14 @@ class CsvFiles(Files):
                          include_processed, CsvFile)
 
     def to_sql_server(self, sql_configuration, use_existing=False, low_memory=True):
+        use_existing=use_existing
         for csv_file in self.__iter__():
             if isinstance(csv_file, CsvFile):
                 csv_file.to_sql_server(
                     pandas_dataframe=csv_file.get_dataframe_utf8_encoding_with_header(low_memory=low_memory),
                     sql_configuration=sql_configuration
                 )
+                use_existing = True  # set to True so we don't keep creating the table.
 
     def get_dataframe_utf8_encoding_with_header(self, column_data_type_dict=None, low_memory=True):
         df_final = pd.DataFrame()
